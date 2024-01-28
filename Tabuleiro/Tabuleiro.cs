4058b27d4249dafc54cd178a9b4deb642469b7b0
vinicius morais araujo <vinidev.morais@gmail.com>
2024-01-28 20:47:12 -0300

    criado excecao personalizada

--- Tabuleiro/Tabuleiro.cs
+++ Tabuleiro/Tabuleiro.cs
@@ -1,3 +1,5 @@
+using Chess_Console.Tabuleiroz;
+
 namespace Chess_Console_Tabuleiroz
 {
     public class Tabuleiro
@@ -16,10 +18,35 @@ namespace Chess_Console_Tabuleiroz
         {
             return Pecas[linha, coluna];
         }
+        public Peca Peca(Posicao pos)
+        {
+            return Pecas[pos.Linha, pos.Coluna];
+        }
+        public bool ExistePeca(Posicao pos)
+        {
+            ValidarPosicao(pos);
+            return Peca(pos) != null;
+        }
         public void ColocarPeca(Peca p, Posicao pos)
         {
             Pecas[pos.Linha, pos.Coluna] = p;
             p.Posicao = pos;
+        }
+        public bool PosicaoValida (Posicao pos)
+        {
+            if (pos.Linha < 0 || pos.Linha >= Linha || pos.Coluna < 0 || pos.Coluna >= Coluna)
+            {
+                return false;
+            }
+            return true;
+        }
+        public void ValidarPosicao(Posicao pos)
+        {
+            if(!PosicaoValida(pos))
+            {
+                throw new TabuleiroException ("Posição inválida"); 
+            }
+
         }
     }
 }
---
+++ Tabuleiro/TabuleiroException.cs
@@ -0,0 +1,9 @@
+namespace Chess_Console.Tabuleiroz
+{
+    public class TabuleiroException : ApplicationException
+    {
+        public TabuleiroException (string mensagem)
+        {
+        }
+    }
+}
