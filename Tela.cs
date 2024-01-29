9bbb44f253516677b3f9e81c2e151cc2daaa4c30
vinicius morais araujo <vinidev.morais@gmail.com>
2024-01-28 21:04:36 -0300

    melhorando o tabuleiro

--- Tela.cs
+++ Tela.cs
@@ -15,11 +15,29 @@ namespace Chess_Console
                     {
                         Console.Write (" -");
                     }
-                    Console.Write($"{tab.Peca(i,j)} ");           
+                    else
+                    {
+                        ImprimirPeca(tab.Peca(i,j));
+                        Console.Write(" ");
+                    }           
                 }
                 System.Console.WriteLine();
             }
             System.Console.WriteLine("  A  B  C  D  E  F  G  H");
+        }
+        public static void ImprimirPeca(Peca peca)
+        {
+            if (peca.Cor == Cor.Branca)
+            {
+                Console.Write(peca);
+            }
+            else
+            {
+                ConsoleColor aux = Console.ForegroundColor;
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.Write(peca);
+                Console.ForegroundColor = aux;
+            }
         }
     }
 }
